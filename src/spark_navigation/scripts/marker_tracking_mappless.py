335a0c3214b887f9903a1d1aa62cd20c04cc1bd4
Baozhe Zhang <907711755@qq.com>
2022-04-26 01:12:26 +0800

    added TF; env added ar_track_alvar OK

--- src/spark_navigation/scripts/marker_tracking_mappless.py
+++ src/spark_navigation/scripts/marker_tracking_mappless.py
@@ -10,19 +10,35 @@ import rospy
 import actionlib
 from actionlib_msgs.msg import GoalStatus
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-import tf, tf2_ros
+from ar_track_alvar_msgs.msg import AlvarMarkers, AlvarMarker # Marker tag
+from geometry_msgs.msg import PoseStamped
+import tf, tf2_ros, tf2_geometry_msgs
 
-def fromCameraToOdo(markerInCamera):
+# Global variables
+IS_STOP = False
+
+def fromCameraToOdom(markerInCamera):
     '''This function makes the transformation from the marker position in 
     /virtual_camera_link to /odom
     
-    markerInCamera: type: TODO
+    markerInCamera: type: AlvarMarkers
     markerInOdom: type: TODO
 
     '''
-
-    markerInOdom = None
-    return markerInOdom
+    # testing code 
+    tf_buffer = tf2_ros.Buffer(rospy.Duration(1200.0))
+    if len(markerInCamera.markers) != 0:
+        for marker in markerInCamera.markers:
+            tf_listener = tf2_ros.TransformListener(tf_buffer)
+            transform = tf_buffer.lookup_transform(
+                "odom", 
+                marker.header.frame_id[1:],
+                rospy.Time(0), 
+                rospy.Duration(1.0))        
+            pose_transformed = tf2_geometry_msgs.do_transform_pose(marker.pose, transform)
+            print(pose_transformed.pose)
+            markerInOdom = pose_transformed
+    # return markerInOdom
 
 def isApproachOK(markerInCamera):
     '''This function returns true if the robot is colse enough with the marker'''
@@ -35,6 +51,7 @@ def sendTheGoal(goal):
     '''This function sends the goal to move_base'''
     return None
 
+
 def checkNewMarker(markers):
     '''Checks if a new marker is detected'''
     newMarkerPose = None
@@ -46,11 +63,10 @@ def generateMoveBaseGoal(markerMsg):
 
 def main():
     rospy.init_node('marker_tracking_mappliess')
+    rospy.Subscriber('/ar_pose_marker', AlvarMarkers, fromCameraToOdom)
+    rospy.spin()
     
-    while not IS_STOP:
-        if IS_ANOTHER_MARKER:
-            # newGoal = transform
-            # send newGoal to approach
-            pass
+if __name__ == "__main__":
+    main()
 
 
