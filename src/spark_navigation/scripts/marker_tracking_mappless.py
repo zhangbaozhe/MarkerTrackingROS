8585c5733d8995c7661d2b6a75ca371b1b588d3e
Baozhe Zhang <907711755@qq.com>
2022-04-26 11:35:27 +0800

    added target marker related declaration

--- src/spark_navigation/scripts/marker_tracking_mappless.py
+++ src/spark_navigation/scripts/marker_tracking_mappless.py
@@ -13,32 +13,54 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from ar_track_alvar_msgs.msg import AlvarMarkers, AlvarMarker # Marker tag
 from geometry_msgs.msg import PoseStamped
 import tf, tf2_ros, tf2_geometry_msgs
+from math import pi
+
+# Global constants
+## id(int) : (lower bound, upper bound)(tuple)
+ENCODED_DIRS = {0: (0, pi/4), 1: (pi/4, pi/2), 2: (pi/2, 3*pi/4), 3: (3*pi/4, pi), 
+                4: (-pi, -3*pi/4), 5: (-3*pi/4, -pi/2), 6: (-pi/2, -pi/4), 7: (-pi/4, 0), 
+                8: None}
 
 # Global variables
 IS_STOP = False
+## This is the global marker pose which lies in odom or (TODO map) 
+## It is the most updated target in each iteration of the navigation process
+POSE_TARGET = AlvarMarker().pose 
+## This is the global target sequence with the last of it as the new target
+TARGET_SEQUENCE = []
+
+def getTargetMarker(markersInCamera):
+    """This function receives the detected markers in the camera frame and add
+    new target in the target sequence
+
+    Args:
+        markersInCamera (AlvarMarkers): The markers in the virtual_camera_link frame
+    """
+    global TARGET_SEQUENCE
+    for marker in markersInCamera.markers:
+        pass
+
 
 def fromCameraToOdom(markerInCamera):
-    '''This function makes the transformation from the marker position in 
-    /virtual_camera_link to /odom
-    
-    markerInCamera: type: AlvarMarkers
-    markerInOdom: type: TODO
+    """Helper function to generate a PoseStamped in the odom (or map) coordinate system
 
-    '''
-    # testing code 
+    Args:
+        markerInCamera (AlvarMaker):
+
+    return:
+        poseInOdom (PoseStamped):
+    """
     tf_buffer = tf2_ros.Buffer(rospy.Duration(1200.0))
-    if len(markerInCamera.markers) != 0:
-        for marker in markerInCamera.markers:
-            tf_listener = tf2_ros.TransformListener(tf_buffer)
-            transform = tf_buffer.lookup_transform(
-                "odom", 
-                marker.header.frame_id[1:],
-                rospy.Time(0), 
-                rospy.Duration(1.0))        
-            pose_transformed = tf2_geometry_msgs.do_transform_pose(marker.pose, transform)
-            print(pose_transformed.pose)
-            markerInOdom = pose_transformed
-    # return markerInOdom
+    tf_listener = tf2_ros.TransformListener(tf_buffer)
+    transform = tf_buffer.lookup_transform(
+        "odom", 
+        markerInCamera.header.frame_id[1:],
+        rospy.Time(0), 
+        rospy.Duration(1.0))        
+    pose_transformed = tf2_geometry_msgs.do_transform_pose(markerInCamera.pose, transform)
+    print("[fromCameraToOdom]", pose_transformed.pose)
+    poseInOdom = pose_transformed
+    return poseInOdom # a PoseStamped 
 
 def isApproachOK(markerInCamera):
     '''This function returns true if the robot is colse enough with the marker'''
@@ -63,7 +85,7 @@ def generateMoveBaseGoal(markerMsg):
 
 def main():
     rospy.init_node('marker_tracking_mappliess')
-    rospy.Subscriber('/ar_pose_marker', AlvarMarkers, fromCameraToOdom)
+    # rospy.Subscriber('/ar_pose_marker', AlvarMarkers, fromCameraToOdom)
     rospy.spin()
     
 if __name__ == "__main__":
